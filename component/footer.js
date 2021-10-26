b1b64dbdf016d8b3825a90d2b421b1ce1ff671b0
swethav08711 <shwe0871@gmail.com>
2021-10-26 12:36:53 +0530

    footer page

---
+++ component/footer.js
@@ -0,0 +1,72 @@
+function footer() {
+  return `<div id="footer">
+  <div id="first_container">
+    <div class="container">
+      <h2>Practo</h2>
+      <p>About</p>
+      <p>Blog</p>
+      <p>Carrers</p>
+      <p>Press</p>
+      <p>Contact Us</p>
+    </div>
+    <div class="container">
+      <h2>For patient</h2>
+      <p>Search for doctors</p>
+      <p>Search for clinics</p>
+      <p>Search for hospitals</p>
+      <p>Book Diagnostic Tests</p>
+      <p>Book full Body Checkups</p>
+      <p>Practo Plus</p>
+      <p>Covid Hospital Tests</p>
+      <p>Practo Plus</p>
+      <p>Covid Hospial listing</p>
+      <p>Book Diagnostic Tests</p>
+      <p>Book full Body Checkups</p>
+      <p>Practo Plus</p>
+      <p>Covid Hospital Tests</p>
+    </div>
+    <div class="container">
+      <h2>For doctors</h2>
+      <p>Practo Profile</p>
+      <p></p>
+      <h2>For clinics</h2>
+      <p>Search for clinics</p>
+      <p>Search for hospitals</p>
+      <p>Book Diagnostic Tests</p>
+      <p>Book full Body Checkups</p>
+    </div>
+    <div class="container">
+      <h2>For hospitals</h2>
+      <p>clinics</p>
+      <p>hospitals</p>
+      <p>Diagnostic Tests</p>
+      <p>Checkups</p>
+    </div>
+    <div class="container">
+      <h2>More</h2>
+      <p>Help</p>
+      <p>Developers</p>
+      <p>Privacy Policy</p>
+      <p>Terms & Conditions</p>
+      <p>Healthcare Directory</p>
+      <p>Practo Health Wiki</p>
+      <p>Corporate Wellness</p>
+    </div>
+    <div class="container">
+      <h2>Social</h2>
+      <p>Facebook</p>
+      <p>Twitter</p>
+      <p>Linkedin</p>
+      <p>Youtube</p>
+      <p>Github</p>
+    </div>
+  </div>
+  <div id="img_container">
+    <img src="/images/practo-footer.5fd476e2.svg" alt="" />
+  </div>
+  <p class="copywrite">
+    Copyright <span>&copy;</span> 2017,Practo.All rights reserved.
+  </p>
+</div> `
+}
+export default footer
---
+++ script/index.js
@@ -0,0 +1,4 @@
+import footer from "../component/footer.js"
+
+const footer_import = document.getElementById("footer_import")
+footer_import.innerHTML = footer()
