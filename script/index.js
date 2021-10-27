e8fb8002dd4ae4d5629e7477ac3c054ffacb8ca1
swethav08711 <shwe0871@gmail.com>
2021-10-27 12:47:10 +0530

    search bar

--- script/index.js
+++ script/index.js
@@ -1,33 +1,31 @@
-import {homebody, footer, header, searchbar } from "../component/export_files.js"
+import { footer, header, homebody } from "../component/export_files.js"
 const footer_import = document.getElementById("footer_import")
 footer_import.innerHTML = footer()
-
 const header_import = document.getElementById("header_import")
 header_import.innerHTML = header()
+
 const body_import = document.getElementById("body_import")
 body_import.innerHTML = homebody()
 
-//slideshow
-var slideIndex = 0;
-    showSlides();
+slideshow
+var slideIndex = 0
+showSlides()
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var i
+  var slides = document.getElementsByClassName("mySlides")
+  var dots = document.getElementsByClassName("dot")
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none"
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  slideIndex++
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "")
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  slides[slideIndex - 1].style.display = "block"
+  dots[slideIndex - 1].className += " active"
+  setTimeout(showSlides, 2000) // Change image every 2 seconds
 }
-
-const search_import = document.getElementById("search_import")
-search_import.innerHTML = searchbar()
-
