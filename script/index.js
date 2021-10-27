7572328c457faacaa264b914d17c0e7d5801b939
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2021-10-27 09:33:16 +0530

    body layout

--- script/index.js
+++ script/index.js
@@ -1,10 +1,33 @@
-import { footer, header, searchbar } from "../component/export_files.js"
-
+import {homebody, footer, header, searchbar } from "../component/export_files.js"
 const footer_import = document.getElementById("footer_import")
 footer_import.innerHTML = footer()
 
 const header_import = document.getElementById("header_import")
 header_import.innerHTML = header()
+const body_import = document.getElementById("body_import")
+body_import.innerHTML = homebody()
+
+//slideshow
+var slideIndex = 0;
+    showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
 const search_import = document.getElementById("search_import")
 search_import.innerHTML = searchbar()
+
