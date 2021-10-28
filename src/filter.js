48c456603a53d40fdf004198e4bd22d6966fdddd
swethav08711 <shwe0871@gmail.com>
2021-10-28 18:41:09 +0530

    total price

--- src/filter.js
+++ src/filter.js
@@ -78,46 +78,7 @@ function addFav(d) {
 window.addEventListener("click", () => {
   box && (results.style.display = "none")
 })
-//filter page navigation
-// var min_app = document.getElementById("filter_side_container")
-// async function getdata(q = "Fitness") {
-//   let res = await fetch(`http://localhost:1212/medicine?q=${q}`)
-//   let data = await res.json()
-//   console.log(data)
-//   append(data)
-// }
-// function append(d) {
-//   d.forEach(({ image, tablet, price }) => {
-//     let div = document.createElement("div")
-//     div.className = "filter_side_container1"
-//     div.addEventListener("click", discription_page)
-//     let img = document.createElement("img")
 
-//     img.src = image
-//     let p = document.createElement("p")
-//     p.innerText = tablet
-//     let pri = document.createElement("p")
-//     pri.className = "price_style_filter"
-//     pri.innerText = `${price}₹`
-//     let btn_div = document.createElement("div")
-//     btn_div.className = "button_contaner"
-//     let buttn1 = document.createElement("button")
-
-//     buttn1.textContent = "--"
-//     let buttn2 = document.createElement("button")
-
-//     buttn2.textContent = "ADD"
-//     btn_div.append(buttn1, buttn2)
-//     div.append(img, p, pri, btn_div)
-//     min_app.append(div)
-//   })
-// }
-// getdata()
-
-// function discription_page() {
-//   window.location.href = "/pages/product_detail.html"
-// }
-//trying
 async function page1() {
   let res = await fetch("http://localhost:1212/medicine?q=Fitness")
   let data = await res.json()
--- src/filter_extract.js
+++ src/filter_extract.js
@@ -19,15 +19,33 @@ function appendchild(d) {
     pri.innerText = `${price}₹`
     let btn_div = document.createElement("div")
     btn_div.className = "button_contaner"
-    let buttn1 = document.createElement("button")
-    buttn1.textContent = "--"
+    pri.innerText = price
+    pri.id = "price"
+    pri.className = "price_cart_cont"
+
+    let div2 = document.createElement("div")
+    div2.className = "quantity button_view_cont"
+
+    let b1 = document.createElement("button")
+    b1.setAttribute("type", "button")
+    b1.className = "btn minus-btn disabled"
+    b1.textContent = "-"
+    let in1 = document.createElement("input")
+    in1.setAttribute("value", "1")
+    in1.setAttribute("type", "text")
+    in1.id = "quantity"
+    let b2 = document.createElement("button")
+    b2.setAttribute("type", "button")
+    b2.className = "btn plus-btn"
+    b2.textContent = "+"
+
     let buttn2 = document.createElement("button")
     buttn2.textContent = "ADD"
     buttn2.onclick = function () {
       addFav({ image, tablet, price })
     }
-
-    btn_div.append(buttn1, buttn2)
+    div2.append(b1, in1, b2)
+    btn_div.append(div2, buttn2)
     div.append(img, p, pri, btn_div)
     filter.append(div)
   })
@@ -50,7 +68,51 @@ function addFav(d) {
   localStorage.setItem("cart", JSON.stringify(addi))
   alert("Added to cart") //back to JSON from Object
   console.log(addi, d)
+
+  let suma = document.getElementById("tot")
+
+  suma.innerText = addi.length
 }
 function viewcart() {
   window.location.href = "/pages/viewcart.html"
 }
+document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
+
+var valueCount
+
+var price = document.getElementById("price").innerText
+
+function priceTotal() {
+  var total = valueCount * price
+  console.log(typeof total)
+  document.getElementById("price").innerText = total
+}
+
+document.querySelector(".plus-btn").addEventListener("click", function () {
+  valueCount = document.getElementById("quantity").value
+
+  valueCount++
+
+  document.getElementById("quantity").value = valueCount
+
+  if (valueCount > 1) {
+    document.querySelector(".minus-btn").removeAttribute("disabled")
+    document.querySelector(".minus-btn").classList.remove("disabled")
+  }
+
+  priceTotal()
+})
+
+document.querySelector(".minus-btn").addEventListener("click", function () {
+  valueCount = document.getElementById("quantity").value
+
+  valueCount--
+
+  document.getElementById("quantity").value = valueCount
+
+  if (valueCount == 1) {
+    document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
+  }
+
+  priceTotal()
+})
