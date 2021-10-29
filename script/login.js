7b76dc36e2d58070069a9e0c7068bc1b34e52486
ketanyadav8807 <ketanyadavbdh8807@gmail.com>
2021-10-29 09:58:06 +0530

    done-login-register-responsivness-and-working-part

--- script/login.js
+++ script/login.js
@@ -5,15 +5,18 @@ var mobile_number_visibuility = document.getElementsByClassName("Mobile-Number-f
 var password_visibuility = document.getElementsByClassName("Password-field-cannot-be-empty")[0];
 var password_label = document.getElementById("password-label");
 
+
 login_button.addEventListener("click" , handle_login);
 
 function handle_login(){
 
-    var mobile_flag = false;
-    var password_flag = false;
+    var mobile_flag = 0;
+    var password_flag = 0;
+    var both_not_found = 0;
     
     mobile_number_visibuility.style.visibility = "hidden";
     password_visibuility.style.visibility = "hidden";
+
     if(login_mobile_number.value == "" || login_password.value == ""){
         if(login_mobile_number.value == ""){
             mobile_number_visibuility.style.visibility = "visible";
@@ -24,6 +27,7 @@ function handle_login(){
         }
     }
     else{
+
     
         fetch("http://localhost:1212/register")
             .then(function(response){
@@ -32,25 +36,57 @@ function handle_login(){
             .then(function(response){
                 for(index in response){
                     if(response[index].mobile_number == login_mobile_number.value || response[index].password == login_password.value){
+                        both_not_found++
                         if(response[index].mobile_number == login_mobile_number.value && response[index].password == login_password.value){
-                            window.location.assign("https://www.practo.com/")
-                        }
-                        else if(response[index].mobile_number == login_mobile_number.value){
-                            mobile_flag = true;
+                            window.location.assign("https://www.practo.com/");
                         }
                         else{
-                            password_flag = true;
+                            if(response[index].mobile_number !== login_mobile_number.value){
+                                mobile_flag++
+                            }
+                            else if(response[index].password !== login_password.value){
+                                password_flag++
+                            }
                         }
+                        
                     }
                       
                 }
 
+                
+            
+                console.log("m" , mobile_flag , password_flag);
+                if(both_not_found == 0){
+                    mobile_number_visibuility.textContent = "Mobile Number is not registered";
+                    mobile_number_visibuility.style.visibility = "visible";
+                }
+                else{
+                    if(mobile_flag !==0 && password_flag !==0){
+                        mobile_number_visibuility.textContent = "Mobile Number is not registered";
+                        mobile_number_visibuility.style.visibility = "visible";
+                    }
+                    else if(mobile_flag>0){
+                        mobile_number_visibuility.textContent = "Mobile Number is not registered";
+                        mobile_number_visibuility.style.visibility = "visible";
+                    }
+                    
+                    else if(password_flag>0){
+                        password_visibuility.textContent = "Incorrect password";
+                        password_visibuility.style.visibility = "visible";
+                    }
+                }
+              
+             
+               
+                console.log( mobile_flag , "p" ,password_flag);
             })
             .catch(function(error){
                 console.log(error)
             })
+            
     }
-    console.log(mobile_flag , password_flag);
+
+
+         
 }
-    login_mobile_number.value = "";
-    login_password.value = "";
+
